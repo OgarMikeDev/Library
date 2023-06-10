21e618ff3704be89f2eed431cb546ece5c07e932
Mike Ogar <ogar.m.v@mail.ru>
2023-06-10 12:22:32 +0300

    "library"

--- src/main/java/com/example/Library/controllers/DefaultController.java
+++ src/main/java/com/example/Library/controllers/DefaultController.java
@@ -1,13 +1,31 @@
 package com.example.Library.controllers;
 
+import com.example.Library.model.Book;
+import com.example.Library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class DefaultController {
+    @Autowired
+    private BookRepository bookRepository;
 
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        Iterable<Book> bookIterable = bookRepository.findAll();
+        List<Book> bookList = new ArrayList<>();
+        for (Book book : bookIterable) {
+            bookList.add(book);
+        }
+
+        model.addAttribute("books", bookList);
+        model.addAttribute("bookCounts", bookList.size());
+
         return "index";
     }
 }
